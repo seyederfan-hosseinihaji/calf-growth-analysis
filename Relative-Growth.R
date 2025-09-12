2e796f46a5365aaaac6a5a5ccb7121e218a90dff
seyederfan-hosseinihaji <seyederfan.hosseinihaji@gmail.com>
2025-09-12 21:23:54 +0330

    Add:initial data frame parameters(ID, Initial_BW, t1 and t2)

---
+++ Relative-Growth.R
@@ -0,0 +1,10 @@
+data_frame <- data.frame(
+  #calves ID
+  ID = c("c1", "c2", "c3", "c4", "c5"),
+  #initial body weight
+  Initial_BW = c(38.2, 42.3, 40, 48, 39),
+  #Day 0
+  t1 = rep(0, 5),
+  #Day 60
+  t2 = rep(60, 5)
+)
