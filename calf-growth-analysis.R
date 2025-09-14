5bfa4b0ec018526f48f2932dea7e4672b9882f3c
seyederfan-hosseinihaji <seyederfan.hosseinihaji@gmail.com>
2025-09-14 22:47:19 +0330

    Add: 1. For loop to determine whether each animal grew 2. Additional comments for clarification 3. Warning message if Initial_BW is less than or equal to 0 in the RGR calculation (line 29)

--- calf-growth-analysis.R
+++ calf-growth-analysis.R
@@ -11,13 +11,34 @@ data_frame <- data.frame(
   t2 = rep(60, 5),
   #relative growth rate
   RGR =  rep(NA, 5),
-  
+  #It shows whether the animal grew by the end of the period or not
   Grew = rep(NA, 5)
 )
 
-# I used a for loop to calculate RGR for each row separately using (i in 1:5).
+# I used a for loop to calculate RGR for each row in data frame separately using (i in 1:5)
 for (i in 1:5) {
-  if (data_frame$Initial_BW[i] > 0 & data_frame$Final_BW[i] > 0 & data_frame$t2[i] > data_frame$t1[i]){
+  #If the initial and final body weights are entered correctly and are greater than 0, and if t1 and t2 are also entered correctly:
+  if (data_frame$Initial_BW[i] > 0 &
+      data_frame$Final_BW[i] > 0 &
+      data_frame$t2[i] > data_frame$t1[i])
+    {
+    #RGR is calculated using the following formula: RGR = (log(Final_BW) - log(Initial_BW)) / (t2 - t1)
     data_frame$RGR[i] = (log(data_frame$Final_BW[i]) - log(data_frame$Initial_BW[i])) / (data_frame$t2[i] - data_frame$t1[i])
+  } else if (data_frame$Initial_BW[i] <= 0) 
+    {
+    print("Initial_BW is less than or equal to 0")
   }
 }
+
+for (i in 1:5) {
+  if (data_frame$Initial_BW[i] > 0 &
+      data_frame$Final_BW[i] > 0 &
+      data_frame$t2[i] > data_frame$t1[i])
+  {
+    data_frame$Grew[i] = isTRUE(data_frame$Initial_BW[i] < data_frame$Final_BW[i] )
+  }
+}
+
+
+
+
